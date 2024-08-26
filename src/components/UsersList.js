14c17edf464265a969a814dccd42bedf2882e373
Emil Tsvetanov <tsvetanovemil87@gmail.com>
2024-08-26 22:25:40 +0300

    Adding removeUser Thunk + UsersListItem Component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -3,8 +3,8 @@ import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
-
 import { useThunk } from "../hooks/use-thunk";
+import UsersListItem from "./UsersListItem";
 
 export default function UsersList() {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] =
@@ -34,13 +34,14 @@ export default function UsersList() {
     content = <div>Error fetching data...</div>;
   } else {
     content = data.map((user) => {
-      return (
-        <div key={user.id} className="mb-2 border rounded">
-          <div className="flex p-2 justify-between items-center cursor-pointer">
-            {user.name}
-          </div>
-        </div>
-      );
+      return <UsersListItem key={user.id} user={user} />;
+      // return (
+      //   <div key={user.id} className="mb-2 border rounded">
+      //     <div className="flex p-2 justify-between items-center cursor-pointer">
+      //       {user.name}
+      //     </div>
+      //   </div>
+      // );
     });
   }
 
